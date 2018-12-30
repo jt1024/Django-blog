29fbc6ceb5a6e17d5a9a47b10c9974c810935565
麦典威 <cctvjiatao@163.com>
2018-12-31 00:07:37 +0800

    complete new login

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^account/', include('account.urls', namespace='account')),
 ]
