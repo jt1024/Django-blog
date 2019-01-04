2c327ceb9d0d1d8584ff9bd546b80d9672e1975b
麦典威 <cctvjiatao@163.com>
2019-01-04 23:47:29 +0800

    complete login and logout

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,13 @@ app_name = 'account'
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name="user_login")
+    # url(r'^login/$', views.user_login, name="user_login"),
+    # url(r'^login/$', LoginView.as_view(), name="user_login"),
+    url(r'^login/$', LoginView.as_view(template_name="account/login.html"), name="user_login"),
+    # url(r'^logout/$', LogoutView.as_view(), name="user_logout"),
+    url(r'^logout/$', LogoutView.as_view(template_name="account/logout.html"), name="user_logout"),
+
 ]
