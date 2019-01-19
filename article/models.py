7817c93cf2a12d3a5b2a15de62aa76d71c1852f7
麦典威 <cctvjiatao@163.com>
2019-01-19 20:33:40 +0800

    add/del tags of user's customed

--- article/models.py
+++ article/models.py
@@ -19,6 +19,14 @@ class ArticleColumn(models.Model):
         return self.column
 
 
+class ArticleTag(models.Model):
+    author = models.ForeignKey(User, related_name="tag", on_delete=models.CASCADE)
+    tag = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.tag
+
+
 class ArticlePost(models.Model):
     author = models.ForeignKey(User, related_name="article", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -28,6 +36,7 @@ class ArticlePost(models.Model):
     created = models.DateTimeField(default=timezone.now())
     updated = models.DateTimeField(auto_now=True)
     users_like = models.ManyToManyField(User, related_name="articles_like", blank=True)
+    article_tag = models.ManyToManyField(ArticleTag, related_name='article_tag', blank=True)
 
     class Meta:
         ordering = ("title",)
